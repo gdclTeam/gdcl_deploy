24cd6ddc6ca0df0195ddf31874347fd1fe0fe7e1
JacksonYANG <353904675@qq.com>
2018-07-26 00:29:09 +0800

    v2.0

--- gdcl/urls.py
+++ gdcl/urls.py
@@ -3,14 +3,35 @@ from . import views
 
 app_name = 'gdcl_index'
 urlpatterns = [
+    # 首页
     url(r'^$', views.index, name='index'),
-    url(r'^works/$', views.works, name='works'),
+
+    # 企业概况五个
     url(r'^description/$', views.description, name='description'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^infomation/$', views.infomation, name='infomation'),
-    url(r'^culture/$', views.culture, name='culture'),
+    url(r'^boss/$', views.boss, name='boss'),
     url(r'^organization/$', views.organization, name='organization'),
     url(r'^certification/$', views.certification, name='certification'),
     url(r'^reputation/$', views.reputation, name='reputation'),
-    url(r'^news/$', views.news, name='news'),
+
+    # 企业文化五个
+    url(r'^logo/$', views.logo, name='logo'),
+    url(r'^management/$', views.management, name='management'),
+    url(r'^show/$', views.show, name='show'),
+    url(r'^video/$', views.video, name='video'),
+    url(r'^employee/$', views.employee, name='employee'),
+
+    # 工程业绩三个
+    url(r'^rewards/$', views.rewards, name='rewards'),
+    url(r'^project_show/$', views.project_show, name='project_show'),
+    url(r'^project_photo/$', views.project_photo, name='project_photo'),
+
+    # 新闻资讯四个
+    url(r'^company_news/$', views.company_news, name='company_news'),
+    url(r'^movement/$', views.movement, name='movement'),
+    url(r'^note/$', views.note, name='note'),
+    url(r'^others/$', views.others, name='others'),
+
+    # 关于我们两个
+    url(r'^recruitment/$', views.recruitment, name='recruitment'),
+    url(r'^contact/$', views.contact, name='contact'),
 ]
--- gdcl/views.py
+++ gdcl/views.py
@@ -6,3 +6,72 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+# 企业概况
+# 企业简介
+def description(request):
+    return render(request, 'description.html')
+# 董事长致辞
+def boss(request):
+    return render(request, 'boss.html')
+# 组织架构
+def organization(request):
+    return render(request, 'organization.html')
+# 公司资质
+def certification(request):
+    return render(request, 'certification.html')
+# 公司荣誉
+def reputation(request):
+    return render(request, 'reputation.html')
+
+# 企业文化
+# 品牌标志
+def logo(request):
+    return render(request, 'logo.html')
+# 管理理念
+def management(request):
+    return render(request, 'management.html')
+# 形象展示
+def show(request):
+    return render(request, 'show.html')
+# 企业视频
+def video(request):
+    return render(request, 'video.html')
+# 员工风采
+def employee(request):
+    return render(request, 'employee.html')
+
+# 工程业绩
+# 获奖工程
+def rewards(request):
+    return render(request, 'rewards.html')
+# 项目展示
+def project_show(request):
+    return render(request, 'project_show.html')
+# 项目实拍
+def project_photo(request):
+    return render(request, 'project_photo.html')
+
+# 新闻资讯
+# 企业新闻
+def company_news(request):
+    return render(request, 'company_news.html')
+# 行业动态
+def movement(request):
+    return render(request, 'movement.html')
+# 通知公告
+def note(request):
+    return render(request, 'note.html')
+# 其他资讯
+def others(request):
+    return render(request, 'others.html')
+
+# 加入我们
+# 人才招聘
+def recruitment(request):
+    return render(request, 'recruitment.html')
+# 联系我们
+def contact(request):
+    return render(request, 'contact.html')
+
+
+
