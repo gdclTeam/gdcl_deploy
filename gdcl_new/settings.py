e546322359436cfbeb3d8fbbf2408323242a0f68
JacksonYANG <353904675@qq.com>
2018-07-28 14:48:20 +0800

    v2.0

--- gdcl_new/settings.py
+++ gdcl_new/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ml(@i&q=*tisd26vuv$up#qaqzmjsoo=w#ny$yf=n10dr@st$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
